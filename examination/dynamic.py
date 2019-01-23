272f8b19d95e3bba0f39f16c522a99af762a983e
Sinyakov Gleb Sergeevich <sinyakov@phystech.edu>
2019-01-23 22:35:01 +0300

    Solve discrete backpack problem

--- examination/dynamic.py
+++ examination/dynamic.py
@@ -22,8 +22,19 @@ def find_longest_cmn_subsequence(s1, s2):
                 arr[i+1][j+1] = max(arr[i+1][j], arr[i][j+1])
     return arr[-1][-1]
 
+
+def backpack_problem(W, w, c):
+    s = [0]*(W+1)
+    maximum = 0
+    for i in range(1, W+1):
+        for wi, ci in zip(w, c):
+            maximum = s[i-wi] + ci if i-wi >= 0 and s[i-wi] + ci > maximum else maximum
+        s[i] = maximum
+    return s[W]
+
 if __name__ == "__main__":
     print(find_longest_incr_subsequence([5, 4, 3, 2, 1, 0]))
     print(find_longest_incr_subsequence([0, 1, 2, 3, 4, 5]))
     print(find_longest_incr_subsequence([1, 2, 1, 7]))
     print(find_longest_cmn_subsequence([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6]))
+    print(backpack_problem(5, [1, 2, 3, 4, 5], [7, 2, 3, 4, 5]))
