e8e5be82acfa1da70d5577abc3a63eca723e4141
Sinyakov Gleb Sergeevich <sinyakov@phystech.edu>
2019-01-23 21:21:40 +0300

    Solve longest common subsequence task

--- examination/dynamic.py
+++ examination/dynamic.py
@@ -11,7 +11,19 @@ def find_longest_incr_subsequence(a):
     return max(b)
 
 
+def find_longest_cmn_subsequence(s1, s2):
+    n, m = len(s1), len(s2)
+    arr = [[0]*(m+1) for _ in range(n+1)]
+    for i in range(n):
+        for j in range(m):
+            if s1[i] == s2[j]:
+                arr[i+1][j+1] = arr[i][j] + 1
+            else:
+                arr[i+1][j+1] = max(arr[i+1][j], arr[i][j+1])
+    return arr[-1][-1]
+
 if __name__ == "__main__":
     print(find_longest_incr_subsequence([5, 4, 3, 2, 1, 0]))
     print(find_longest_incr_subsequence([0, 1, 2, 3, 4, 5]))
     print(find_longest_incr_subsequence([1, 2, 1, 7]))
+    print(find_longest_cmn_subsequence([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6]))
